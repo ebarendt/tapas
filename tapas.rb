bb8bf4c645e3c88d860adbe119acf7d3a9e29895
Eric Barendt <ebarendt@collab.net>
2012-11-28 10:24:34 -0800

    Working feed downloader

--- tapas.rb
+++ tapas.rb
@@ -3,33 +3,6 @@
 require 'faraday'
 require 'nokogiri'
 
-class BasicServer
-
-  attr_accessor :server
-  attr_accessor :username
-  attr_accessor :password
-
-  def initialize(server, username, password)
-    @server = server
-    @username = username
-    @password = password
-  end
-
-  def conn
-    @conn || Faraday.new(:url => server.to_s) do |faraday|
-      faraday.basic_auth username, password
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
-  end
-
-  def get(path)
-    conn.get(path)
-  end
-
-end
-
 class TapasServer
 
   attr_accessor :server
@@ -42,64 +15,80 @@ class TapasServer
     @password = password
   end
 
-  def conn
-    @conn || Faraday.new(:url => server.to_s) do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
+  def fetch_feed
+    conn(true).get('/feed').body
   end
 
-  def authenticate
-    response = conn.post '/subscriber/login', { username: username, password: password }
-    @cookie = response.headers['set-cookie']
+  def save_file(directory, file_name, file_path)
+    puts "Saving to #{file_name}"
+    response = get(file_path)
+    file_data = response.body
+    save_path = File.join(directory, file_name)
+    IO.write(save_path, file_data)
   end
+
+  private
 
   def get(path)
+    authenticate
     conn.get(path) do |req|
       req.headers['Cookie'] = @cookie
     end
   end
 
-  def save_file(path)
-    response = get(path)
-    disposition = response.headers['content-disposition']
-    filename = Faraday::Utils.parse_query(disposition)["filename"].gsub(/\"/, '')
-    file_data = response.body
-    IO.write(filename, file_data)
+  def authenticate
+    @cookie ||= conn.post('/subscriber/login', { username: username, password: password }).headers['set-cookie']
   end
 
+  def conn(basic = false)
+    @conn ||= {}
+    @conn[basic] ||= Faraday.new(:url => server.to_s) do |faraday|
+      faraday.basic_auth(username, password) if basic
+      faraday.request  :url_encoded             # form-encode POST params
+      # faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+  end
+
+end
+
+def extract_links_to_download(server, feed)
+  xml_doc = Nokogiri::XML(feed)
+  items = xml_doc.xpath('//item')
+  items.map do |item|
+    children = item.children
+    title = (children / 'title').first.child.content
+    description = (children / 'description').first.child.content
+    description_doc = Nokogiri::HTML(description)
+    links = description_doc.css('ul li a')
+
+    links_to_download = links.each_with_object([]) do |link, memo|
+      if link['href'] =~ /rubytapas.dpdcart.com\/subscriber\/download/
+        memo << { filename: link.content, link: link['href'].gsub(/#{server}/, '') }
+      end
+    end
+
+    { title: title, links: links_to_download }
+  end
+end
+
+def download_episode(episode)
+  return if episode[:links].empty? || File.directory?(episode[:title])
+
+  puts "Downloading #{episode[:title]}..."
+  Dir.mkdir(episode[:title])
+  episode[:links].each do |link|
+    tapas_server.save_file(episode[:title], link[:filename], link[:link])
+  end
 end
 
 username = ARGV[0]
 password = ARGV[1]
-uri = URI(ARGV[2])
-server = URI::Generic.build(scheme: uri.scheme, host: uri.host, port: uri.port)
-file_url = "#{uri.path}?#{uri.query}"
+server = "https://rubytapas.dpdcart.com"
 
-#feed_server = BasicServer.new(server, username, password)
-#rss_feed = feed_server.get('/feed').body
 
-#xml_doc = Nokogiri::XML(rss_feed)
-xml_doc = Nokogiri::XML(IO.read('feed.xml'))
-items = xml_doc.xpath('//item')
-items.each do |item|
-  children = item.children
-  title = (children / 'title').first.child.content
-  description = (children / 'description').first.child.content
-  description_doc = Nokogiri::HTML(description)
-  links = description_doc.css('ul li a')
-
-  links_to_download = links.each_with_object([]) do |link, memo|
-    if link['href'] =~ /rubytapas.dpdcart.com\/subscriber\/download/
-      memo << { filename: link.content, link: link['href'] }
-    end
-  end
-
-  puts title
-  puts links_to_download
+tapas_server = TapasServer.new(server, username, password)
+feed = tapas_server.fetch_feed
+extract_links_to_download(server, feed).each do |episode|
+  download_episode(episode)
 end
-
-#tapas_server = TapasServer.new(server, username, password)
-#tapas_server.authenticate
-#tapas_server.save_file(file_url)
